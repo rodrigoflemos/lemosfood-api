71b950f4d4ade61de46712741eafed957a7a6cb9
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-13 19:15:15 -0300

    Aula 13.8
    Implementando paginação e ordenação em recursos de coleção da API

--- src/main/java/br/com/lemos/lemosfood/api/controller/CozinhaController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/CozinhaController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,10 +44,13 @@ public class CozinhaController {
 	private CozinhaInputDisassembler cozinhaInputDisassembler;   
 	
 	@GetMapping
-	public List<CozinhaModel> listar() {
-	    List<Cozinha> todasCozinhas = cozinhaRepository.findAll();
+	public Page<CozinhaModel> listar(Pageable pageable) {
+	    Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+	 
+	    List<CozinhaModel> cozinhasModel = cozinhaModelAssembler.toCollectionModel(cozinhasPage.getContent());
 	    
-	    return cozinhaModelAssembler.toCollectionModel(todasCozinhas);
+	    Page<CozinhaModel> cozinhasModelPage = new PageImpl<>(cozinhasModel, pageable, cozinhasPage.getTotalElements());
+	    return cozinhasModelPage;
 	}
 
 	@GetMapping("/{cozinhaId}")
