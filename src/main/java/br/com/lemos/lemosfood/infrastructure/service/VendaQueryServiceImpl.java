043d3311a5365143e8b1f076936d624822934edd
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-16 15:55:36 -0300

    Aula 13.14
    Implementando consulta com dados agregados de vendas diárias

--- src/main/java/br/com/lemos/lemosfood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/br/com/lemos/lemosfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,18 +1,42 @@
 package br.com.lemos.lemosfood.infrastructure.service;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import br.com.lemos.lemosfood.domain.filter.VendaDiariaFilter;
+import br.com.lemos.lemosfood.domain.model.Pedido;
 import br.com.lemos.lemosfood.domain.model.dto.VendaDiaria;
 import br.com.lemos.lemosfood.domain.service.VendaQueryService;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService{
 
+	@Autowired
+	EntityManager manager;
 	
 	@Override
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-		// TODO Auto-generated method stub
-		return null;
+		var builder = manager.getCriteriaBuilder();
+		var query = builder.createQuery(VendaDiaria.class);
+		var root = query.from(Pedido.class);
+		
+		var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
+		
+		var selection = builder.construct(VendaDiaria.class, 
+				functionDateDataCriacao, 
+				builder.count(root.get("id")),
+				builder.sum(root.get("valorTotal"))
+				);
+		
+		query.select(selection);
+		query.groupBy(functionDateDataCriacao);
+		
+		return manager.createQuery(query).getResultList();
 	}
 
 }
