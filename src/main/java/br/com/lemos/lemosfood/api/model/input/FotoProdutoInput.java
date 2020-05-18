565db97cde23d27e32dd79706767a054258cdd49
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-18 19:15:06 -0300

    Aula 14.1 e 14.2
    Conhecendo soluções para upload de arquivos em REST APIs
    Implementando upload de arquivo com multipart/form-data

---
+++ src/main/java/br/com/lemos/lemosfood/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,14 @@
+package br.com.lemos.lemosfood.api.model.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	private MultipartFile arquivo;
+	private String descricao;
+}
