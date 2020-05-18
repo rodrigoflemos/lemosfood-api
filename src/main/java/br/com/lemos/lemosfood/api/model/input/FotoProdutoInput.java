9338ae51e8dde3c095fd5febf4f4e32b01df1e38
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-18 19:51:43 -0300

    Aula 14.4
    Desafio: Validando o content type do arquivo

--- src/main/java/br/com/lemos/lemosfood/api/model/input/FotoProdutoInput.java
+++ src/main/java/br/com/lemos/lemosfood/api/model/input/FotoProdutoInput.java
@@ -3,8 +3,10 @@ package br.com.lemos.lemosfood.api.model.input;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.lemos.lemosfood.core.validation.FileContentType;
 import br.com.lemos.lemosfood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +17,7 @@ public class FotoProdutoInput {
 
 	@NotNull
 	@FileSize(max = "100KB")
+	@FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
 	private MultipartFile arquivo;
 	
 	@NotBlank
