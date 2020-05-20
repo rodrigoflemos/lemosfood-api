8dbac806506c963caa3439d591f0695d3ccd19c8
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-20 17:10:14 -0300

    Aula 14.14
    Checando media type ao servir arquivos de fotos - Correção

--- src/main/java/br/com/lemos/lemosfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/RestauranteProdutoFotoController.java
@@ -70,6 +70,7 @@ public class RestauranteProdutoFotoController {
 	    return fotoProdutoModelAssembler.toModel(fotoProduto);
 	}
 	
+	@GetMapping
 	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, 
 	        @PathVariable Long produtoId,  @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
 		
