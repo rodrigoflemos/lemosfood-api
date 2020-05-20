4c7075042ea5eb50d7cc7aef027d2c5ea73d6643
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-20 15:05:59 -0300

    Aula 14.13
    Servindo arquivo de fotos pela API

--- src/main/java/br/com/lemos/lemosfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/RestauranteProdutoFotoController.java
@@ -1,11 +1,14 @@
 package br.com.lemos.lemosfood.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,10 +19,12 @@ import org.springframework.web.multipart.MultipartFile;
 import br.com.lemos.lemosfood.api.assembler.FotoProdutoModelAssembler;
 import br.com.lemos.lemosfood.api.model.FotoProdutoModel;
 import br.com.lemos.lemosfood.api.model.input.FotoProdutoInput;
+import br.com.lemos.lemosfood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.lemos.lemosfood.domain.model.FotoProduto;
 import br.com.lemos.lemosfood.domain.model.Produto;
 import br.com.lemos.lemosfood.domain.service.CadastroProdutoService;
 import br.com.lemos.lemosfood.domain.service.CatalogoFotoProdutoService;
+import br.com.lemos.lemosfood.domain.service.FotoStorageService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
@@ -33,6 +38,9 @@ public class RestauranteProdutoFotoController {
 	
 	@Autowired
 	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+	
+	@Autowired
+	private FotoStorageService fotoStorage;
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
@@ -51,11 +59,26 @@ public class RestauranteProdutoFotoController {
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public FotoProdutoModel buscar(@PathVariable Long restauranteId, 
 	        @PathVariable Long produtoId) {
 	    FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
 	    
 	    return fotoProdutoModelAssembler.toModel(fotoProduto);
+	}
+	
+	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, 
+	        @PathVariable Long produtoId) {
+		
+		try {
+			FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+			InputStream inputStream = fotoStorage.recuperar(fotoProduto.getNomeArquivo());
+			return ResponseEntity.ok()
+					.contentType(MediaType.IMAGE_JPEG)
+					.body(new InputStreamResource(inputStream));
+		}catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build(); 
+		}
 	}
 }
