3726cb1b93ba23e2b0387b0fc99f33988f4951af
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-21 14:45:18 -0300

    Aula 14.22
    Definindo bean do client da Amazon S3 e configurando credenciais

--- src/main/java/br/com/lemos/lemosfood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/br/com/lemos/lemosfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,12 +2,18 @@ package br.com.lemos.lemosfood.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 import br.com.lemos.lemosfood.domain.service.FotoStorageService;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
+	
+	@Autowired
+	private AmazonS3 amazonS3;
 
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
