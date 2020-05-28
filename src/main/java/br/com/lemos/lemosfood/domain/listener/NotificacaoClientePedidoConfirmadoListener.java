722586fe8d3ccca60b010edf2cac6ead644a0d4a
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-28 19:01:21 -0300

    Aula 15.12
    Observando e reagindo a Domain Events

---
+++ src/main/java/br/com/lemos/lemosfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -0,0 +1,33 @@
+package br.com.lemos.lemosfood.domain.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import br.com.lemos.lemosfood.domain.event.PedidoConfirmadoEvent;
+import br.com.lemos.lemosfood.domain.model.Pedido;
+import br.com.lemos.lemosfood.domain.service.EnvioEmailService;
+import br.com.lemos.lemosfood.domain.service.EnvioEmailService.Mensagem;
+
+@Component
+public class NotificacaoClientePedidoConfirmadoListener {
+
+	@Autowired
+	private EnvioEmailService envioEmail;
+	
+	@EventListener
+	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+		
+		Pedido pedido = event.getPedido();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("pedido-confirmado.html")
+				.variavel("pedido", pedido)
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmail.enviar(mensagem);
+	}
+	
+}
