b87b4d29aa561a0e23c76acbc71acd29e9fc7bff
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-05-29 10:28:44 -0300

    Aula 15.13
    Reagindo a Domain Events em fases específicas da transação

--- src/main/java/br/com/lemos/lemosfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/br/com/lemos/lemosfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,8 +1,9 @@
 package br.com.lemos.lemosfood.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import br.com.lemos.lemosfood.domain.event.PedidoConfirmadoEvent;
 import br.com.lemos.lemosfood.domain.model.Pedido;
@@ -15,7 +16,7 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 	
-	@EventListener
+	@TransactionalEventListener
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		
 		Pedido pedido = event.getPedido();
@@ -29,5 +30,4 @@ public class NotificacaoClientePedidoConfirmadoListener {
 		
 		envioEmail.enviar(mensagem);
 	}
-	
 }
