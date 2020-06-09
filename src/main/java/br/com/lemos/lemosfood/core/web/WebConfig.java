1b7710d2aa7b387ebcb105b9d013bc6de1544021
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-06-09 16:34:25 -0300

    Aula 17.5
    Implementando requisições condicionais com Shallow ETags

--- src/main/java/br/com/lemos/lemosfood/core/web/WebConfig.java
+++ src/main/java/br/com/lemos/lemosfood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package br.com.lemos.lemosfood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer{
 			.allowedMethods("*");
 //			.allowedOrigins("")
 //			.maxAge(30);
+	}
+	
+	@Bean
+	public Filter shallowETagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
