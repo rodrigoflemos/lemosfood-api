ec4e87cdc5d23e54afdf2179d1136578917fd44a
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-06-11 16:00:16 -0300

    Aula 17.6
    Adicionando outras diretivas de Cache-Control na resposta HTTP

--- src/main/java/br/com/lemos/lemosfood/api/controller/FormaPagamentoController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/FormaPagamentoController.java
@@ -50,7 +50,11 @@ public class FormaPagamentoController {
         List<FormaPagamentoModel> formasPagamentoModel = formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
         
         return ResponseEntity.ok()
-        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+//        		.cacheControl(CacheControl.noCache())
+//        		.cacheControl(CacheControl.noStore())
         		.body(formasPagamentoModel);
     }
     
