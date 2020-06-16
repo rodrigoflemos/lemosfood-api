db22db8431d8254b7bca99ee2ac9edb1b936b7a3
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-06-16 15:45:33 -0300

    Aula 18.6
    Descrevendo informações da API na documentação

--- src/main/java/br/com/lemos/lemosfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/lemos/lemosfood/core/openapi/SpringFoxConfig.java
@@ -1,10 +1,15 @@
 package br.com.lemos.lemosfood.core.openapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,11 +18,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SpringFoxConfig implements WebMvcConfigurer{
 
+	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-					.apis(RequestHandlerSelectors.basePackage("br.com.lemosfood.api"))
-					.build();
+					.apis(RequestHandlerSelectors.basePackage("br.com.lemos.lemosfood.api"))
+					.paths(PathSelectors.any())
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("LemosFood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("LemosFood", "https://www.lemosfood.com", "contato@lemosfood.com"))
+				.build();
 	}
 	
 	@Override
