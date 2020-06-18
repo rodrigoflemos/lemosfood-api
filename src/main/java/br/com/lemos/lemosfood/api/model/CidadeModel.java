dd353948f9215f6f6d471a9dec78baa71de3235d
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-06-18 18:52:08 -0300

    Aula 18.10
    Descrevendo modelos de representações e suas propriedades

--- src/main/java/br/com/lemos/lemosfood/api/model/CidadeModel.java
+++ src/main/java/br/com/lemos/lemosfood/api/model/CidadeModel.java
@@ -1,13 +1,22 @@
 package br.com.lemos.lemosfood.api.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+//@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Setter
 @Getter
 public class CidadeModel {
 
+//	@ApiModelProperty(value = "ID da cidade", example = "1")
+	@ApiModelProperty(example = "1")
+
     private Long id;
+	
+	@ApiModelProperty(example = "São Paulo")
     private String nome;
+	
     private EstadoModel estado;
 }       
