86c3955ee63924baf3307995d55c912a11bddd2f
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-06-28 15:39:44 -0300

    Aula 18.39
    Desafio: descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/br/com/lemos/lemosfood/api/controller/UsuarioGrupoController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/UsuarioGrupoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lemos.lemosfood.api.assembler.GrupoModelAssembler;
 import br.com.lemos.lemosfood.api.model.GrupoModel;
+import br.com.lemos.lemosfood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import br.com.lemos.lemosfood.domain.model.Usuario;
 import br.com.lemos.lemosfood.domain.service.CadastroUsuarioService;
 
 @RestController
-@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos", 
+    produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
