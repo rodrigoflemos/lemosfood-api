cb7303d8253067f49e61f182657086585e3c5314
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-07-14 10:57:58 -0300

    Aula 19.8
    Construindo links dinâmicos com WebMvcLinkBuilder

--- src/main/java/br/com/lemos/lemosfood/api/controller/CidadeController.java
+++ src/main/java/br/com/lemos/lemosfood/api/controller/CidadeController.java
@@ -1,11 +1,12 @@
 package br.com.lemos.lemosfood.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -61,13 +62,17 @@ public class CidadeController implements CidadeControllerOpenApi {
 		
 		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
 		
-		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades/1"));
-//		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades/1", IanaLinkRelations.SELF));
+		cidadeModel.add(linkTo(CidadeController.class)
+				.slash(cidadeModel.getId()).withSelfRel());
 		
-//		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades", IanaLinkRelations.COLLECTION));
-		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades", "cidades"));
+//		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades/1"));
+
+//		cidadeModel.add(new Link("http://api.algafood.local:8080/cidades", "cidades"));
+
+		cidadeModel.add(linkTo(CidadeController.class).withRel("cidades"));
 		
-		cidadeModel.getEstado().add(new Link("http://api.algafood.local:8080/estados/1"));
+		cidadeModel.getEstado().add(linkTo(EstadoController.class)
+				.slash(cidadeModel.getEstado().getId()).withSelfRel());
 		
 		return cidadeModel;
 	}
