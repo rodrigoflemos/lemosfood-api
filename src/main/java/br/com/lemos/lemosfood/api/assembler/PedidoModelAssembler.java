d3b92cfe54a01d8c31e5e190e87e83c153b6f338
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-04 10:12:03 -0300

    Aula 19.20
    Refatorando construção e inclusão de links em representation model

--- src/main/java/br/com/lemos/lemosfood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/br/com/lemos/lemosfood/api/assembler/PedidoModelAssembler.java
@@ -5,14 +5,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import br.com.lemos.lemosfood.api.LemosLinks;
 import br.com.lemos.lemosfood.api.controller.CidadeController;
 import br.com.lemos.lemosfood.api.controller.FormaPagamentoController;
 import br.com.lemos.lemosfood.api.controller.PedidoController;
@@ -28,6 +24,9 @@ public class PedidoModelAssembler
 
     @Autowired
     private ModelMapper modelMapper;
+    
+    @Autowired
+    private LemosLinks lemosLinks;
 
     public PedidoModelAssembler() {
         super(PedidoController.class, PedidoModel.class);
@@ -38,21 +37,7 @@ public class PedidoModelAssembler
         PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModel);
         
-        TemplateVariables pageVariables = new TemplateVariables(
-        		new TemplateVariable("page", VariableType.REQUEST_PARAM),
-        		new TemplateVariable("size", VariableType.REQUEST_PARAM),
-        		new TemplateVariable("sort", VariableType.REQUEST_PARAM)
-        		);
-        
-        TemplateVariables filtroVariables = new TemplateVariables(
-        		new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-        		new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-        		new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-        		new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
-        
-        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-        
-        pedidoModel.add(new Link(UriTemplate.of(pedidosUrl, pageVariables.concat(filtroVariables)), "pedidos"));
+        pedidoModel.add(lemosLinks.linkToPedidos());
         
 //        pedidoModel.add(linkTo(PedidoController.class).withRel("pedidos"));
         
