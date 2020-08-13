ed574959ee4f4d3d98af8336d3b6a43df652f635
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-13 14:27:07 -0300

    Aula 19.40
    Corrigindo a documentação dos endpoints de cidades

---
+++ src/main/java/br/com/lemos/lemosfood/api/openapi/model/CidadesModelOpenApi.java
@@ -0,0 +1,24 @@
+package br.com.lemos.lemosfood.api.openapi.model;
+
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
+import br.com.lemos.lemosfood.api.model.CidadeModel;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+@ApiModel("CidadesModel")
+@Data
+public class CidadesModelOpenApi {
+
+	private CidadeEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	
+	@ApiModel("CidadesEmbeddedModel")
+	@Data
+	public class CidadeEmbeddedModelOpenApi {
+	
+		private List<CidadeModel> cidades;
+	}
+}
