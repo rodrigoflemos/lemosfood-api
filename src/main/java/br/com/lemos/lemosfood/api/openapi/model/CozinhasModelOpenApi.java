95d5c9f01498489b286273c3725d4147447ed36a
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-13 14:59:24 -0300

    Aula 19.41
    Corrigindo a paginação na documentação

--- src/main/java/br/com/lemos/lemosfood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/br/com/lemos/lemosfood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,8 +1,31 @@
 package br.com.lemos.lemosfood.api.openapi.model;
 
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
+import br.com.lemos.lemosfood.api.model.CidadeModel;
 import br.com.lemos.lemosfood.api.model.CozinhaModel;
+import br.com.lemos.lemosfood.api.openapi.model.CidadesModelOpenApi.CidadeEmbeddedModelOpenApi;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel>{
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+	
+	private CozinhasEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	private PageModelOpenApi page;
+	
+	@ApiModel("CozinhasEmbeddedModel")
+	@Data
+	public class CozinhasEmbeddedModelOpenApi {
+	
+		private List<CozinhaModel> cozinhas;
+	}
+	
 }
