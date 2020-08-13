2ee2621e41e5f9b3e7a3087ba6bf62abdb879dcd
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-13 12:33:09 -0300

    Aula 19.39
    Corrigindo as propriedades de links na documentação

--- src/main/java/br/com/lemos/lemosfood/core/springfox/SpringFoxConfig.java
+++ src/main/java/br/com/lemos/lemosfood/core/springfox/SpringFoxConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -26,6 +27,7 @@ import br.com.lemos.lemosfood.api.exceptionhandler.Problem;
 import br.com.lemos.lemosfood.api.model.CozinhaModel;
 import br.com.lemos.lemosfood.api.model.PedidoResumoModel;
 import br.com.lemos.lemosfood.api.openapi.model.CozinhasModelOpenApi;
+import br.com.lemos.lemosfood.api.openapi.model.LinksModelOpenApi;
 import br.com.lemos.lemosfood.api.openapi.model.PageableModelOpenApi;
 import br.com.lemos.lemosfood.api.openapi.model.PedidosResumoModelOpenApi;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -69,6 +71,7 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 	                    URL.class, URI.class, URLStreamHandler.class, Resource.class,
 	                    File.class, InputStream.class)
 	            .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+	            .directModelSubstitute(Links.class, LinksModelOpenApi.class)
 	            .alternateTypeRules(AlternateTypeRules.newRule(
 	                    typeResolver.resolve(Page.class, CozinhaModel.class),
 	                    CozinhasModelOpenApi.class))
