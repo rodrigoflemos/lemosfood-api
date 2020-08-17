362f66826a921f45c83d4f4ac57d0b13b6eaacdc
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-17 10:29:09 -0300

    Aula 20.19
    Desligando uma versão da API

--- src/main/java/br/com/lemos/lemosfood/core/web/ApiRetirementHandler.java
+++ src/main/java/br/com/lemos/lemosfood/core/web/ApiRetirementHandler.java
@@ -3,20 +3,20 @@ package br.com.lemos.lemosfood.core.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component
-public class ApiDeprecationHandler extends HandlerInterceptorAdapter{
+public class ApiRetirementHandler extends HandlerInterceptorAdapter{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
 		if(request.getRequestURI().startsWith("/v1/")) {
-			response.addHeader("X-LemosFood-Deprecated",
-					"Essa versão da API está depreciada e deixará de existir a partir de 01/01/2021. "
-							+ "Use a versão mais atual da API." );
+			response.setStatus(HttpStatus.GONE.value());
+			return false;
 		}
 		
 		return true;
