2deb55c9152de6111f55f8f4bc8b372f5d39f8e3
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-21 20:12:33 -0300

    Aula 22.11
    Configurando o Resource Server com a nova stack do Spring Security

---
+++ src/main/java/br/com/lemos/lemosfood/core/security/ResourceSecurityConfig.java
@@ -0,0 +1,20 @@
+package br.com.lemos.lemosfood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated() //Autoriza qualquer requisicao autenticada
+			.and()
+			.oauth2ResourceServer().opaqueToken();
+	}
+}
