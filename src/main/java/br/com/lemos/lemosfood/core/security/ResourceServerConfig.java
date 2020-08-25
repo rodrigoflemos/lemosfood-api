7a0ae608189cf2b4b2a84c7ee40e60b5e9372aa0
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-25 11:40:08 -0300

    Aula 23.6
    Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/br/com/lemos/lemosfood/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/lemos/lemosfood/core/security/ResourceServerConfig.java
@@ -1,13 +1,18 @@
 package br.com.lemos.lemosfood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
-public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -16,6 +21,13 @@ public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest().authenticated() //Autoriza qualquer requisicao autenticada
 			.and()
 			.cors().and() //Resolve problema de autenticacao ao acessar OPTIONS
-			.oauth2ResourceServer().opaqueToken();
+			.oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		
+		var secretKey = new SecretKeySpec("as9d879sdf7asdf78a9dsadf87987fd89s87fs9a987s98f".getBytes(), "HmacSHA256");
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 }
