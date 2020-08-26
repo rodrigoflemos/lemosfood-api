a082b820886403bdb651fc4dfaea7d8f0c3b0a35
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-08-26 14:26:40 -0300

    Aula 23.12
    Configurando a validação de JWT no Resource Server com a chave pública

--- src/main/java/br/com/lemos/lemosfood/core/security/ResourceServerConfig.java
+++ src/main/java/br/com/lemos/lemosfood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package br.com.lemos.lemosfood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +17,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.cors().and() //Resolve problema de autenticacao ao acessar OPTIONS
 			.oauth2ResourceServer().jwt();
-	}
-	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		
-		var secretKey = new SecretKeySpec("as9d879sdf7asdf78a9dsadf87987fd89s87fs9a987s98f".getBytes(), "HmacSHA256");
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 }
