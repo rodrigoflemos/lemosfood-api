cd2ddebeb1844731c29f3e6e1bb162d7327ad7cf
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-09-02 19:00:28 -0300

    Aula 23.41
    Juntando o Resource Server com o Authorization Server no mesmo projeto

---
+++ src/main/java/br/com/lemos/lemosfood/core/security/authorizationserver/JwtKeyStoreProperties.java
@@ -0,0 +1,29 @@
+package br.com.lemos.lemosfood.core.security.authorizationserver;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Validated
+@Component
+@ConfigurationProperties("lemosfood.jwt.keystore")
+public class JwtKeyStoreProperties {
+
+    @NotNull
+    private Resource jksLocation;
+    
+    @NotBlank
+    private String password;
+    
+    @NotBlank
+    private String keypairAlias;
+}
