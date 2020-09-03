a70a628fe4f27fdac74b46eb68429aadbe0a0503
Rodrigo Lemos <rodrigoflemos@gmail.com>
2020-09-03 17:11:48 -0300

    Aula 23.43
    Customizando a página de login

---
+++ src/main/java/br/com/lemos/lemosfood/core/security/authorizationserver/SecurityController.java
@@ -0,0 +1,13 @@
+package br.com.lemos.lemosfood.core.security.authorizationserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "pages/login";
+	}
+}
